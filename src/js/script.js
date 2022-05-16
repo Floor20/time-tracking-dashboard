577ca5c2f02f464093cf8de74868cae98ce9817b
Floor20 <austino2112@gmail.com>
2022-05-16 17:54:27 -0600

    Completed w/external JSON

--- src/js/script.js
+++ src/js/script.js
@@ -111,52 +111,77 @@ const setupTabs = function () {
         }
       ]
       `;
-      const timeData = JSON.parse(timeJSONdata);
+      // const timeData = JSON.parse(timeJSONdata);
+      // const timeData = require("../../data.json");
 
-      timeData.forEach((element, index) => {
-        let dailyTime = element.timeframes.daily.current;
-        let lastDailyTime = element.timeframes.daily.previous;
-        let weeklyTime = element.timeframes.weekly.current;
-        let lastWeeklyTime = element.timeframes.weekly.previous;
-        let monthlyTime = element.timeframes.monthly.current;
-        let lastMonthlyTime = element.timeframes.monthly.previous;
-        console.log(timeRows);
-        console.log(dailyTime, lastDailyTime);
+      const getTimeData = async function () {
+        try {
+          const res = await fetch("../../data.json");
 
-        const [dailyTimeMarkup, weeklyTimeMarkup, monthlyTimeMarkup] = [
-          `
-          <ul class="tile__time-row">
-            <li class="tile__time" data-time="${tab}">${dailyTime}hrs</li>
-            <li class="tile__time-last" data-time-last="${tab}">Yesterday - ${lastDailyTime}hrs
-            </li>
-          </ul>
-          `,
-          `
-          <ul class="tile__time-row">
-            <li class="tile__time" data-time="${tab}">${weeklyTime}hrs</li>
-            <li class="tile__time-last" data-time-last="${tab}">Last week - ${lastWeeklyTime}hrs
-            </li>
-          </ul>
-          `,
-          `
-          <ul class="tile__time-row">
-            <li class="tile__time" data-time="${tab}">${monthlyTime}hrs</li>
-            <li class="tile__time-last" data-time-last="${tab}">Last month - ${lastMonthlyTime}hrs
-            </li>
-          </ul>
-          `,
-        ];
+          if (!res.ok) throw new Error("Problem getting advice data");
 
-        timeRows[index].classList.remove("tile__time-row");
-        timeRows[index].classList.add("tile__time--hidden");
+          const timeData = await res.json();
 
-        if (tab === "1")
-          timeRows[index].insertAdjacentHTML("beforebegin", dailyTimeMarkup);
-        if (tab === "2")
-          timeRows[index].insertAdjacentHTML("beforebegin", weeklyTimeMarkup);
-        if (tab === "3")
-          timeRows[index].insertAdjacentHTML("beforebegin", monthlyTimeMarkup);
-      });
+          timeData.forEach((element, index) => {
+            let dailyTime = element.timeframes.daily.current;
+            let lastDailyTime = element.timeframes.daily.previous;
+            let weeklyTime = element.timeframes.weekly.current;
+            let lastWeeklyTime = element.timeframes.weekly.previous;
+            let monthlyTime = element.timeframes.monthly.current;
+            let lastMonthlyTime = element.timeframes.monthly.previous;
+            console.log(timeRows);
+            console.log(dailyTime, lastDailyTime);
+
+            const [dailyTimeMarkup, weeklyTimeMarkup, monthlyTimeMarkup] = [
+              `
+              <ul class="tile__time-row">
+                <li class="tile__time" data-time="${tab}">${dailyTime}hrs</li>
+                <li class="tile__time-last" data-time-last="${tab}">Yesterday - ${lastDailyTime}hrs
+                </li>
+              </ul>
+              `,
+              `
+              <ul class="tile__time-row">
+                <li class="tile__time" data-time="${tab}">${weeklyTime}hrs</li>
+                <li class="tile__time-last" data-time-last="${tab}">Last week - ${lastWeeklyTime}hrs
+                </li>
+              </ul>
+              `,
+              `
+              <ul class="tile__time-row">
+                <li class="tile__time" data-time="${tab}">${monthlyTime}hrs</li>
+                <li class="tile__time-last" data-time-last="${tab}">Last month - ${lastMonthlyTime}hrs
+                </li>
+              </ul>
+              `,
+            ];
+
+            timeRows[index].classList.remove("tile__time-row");
+            timeRows[index].classList.add("tile__time--hidden");
+
+            if (tab === "1")
+              timeRows[index].insertAdjacentHTML(
+                "beforebegin",
+                dailyTimeMarkup
+              );
+            if (tab === "2")
+              timeRows[index].insertAdjacentHTML(
+                "beforebegin",
+                weeklyTimeMarkup
+              );
+            if (tab === "3")
+              timeRows[index].insertAdjacentHTML(
+                "beforebegin",
+                monthlyTimeMarkup
+              );
+          });
+        } catch (err) {
+          console.error(`${err.message}`);
+          alert(err.message);
+        }
+      };
+
+      getTimeData();
     });
   });
 };
